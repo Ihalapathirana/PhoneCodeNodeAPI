4215865abdeea21febe87879d410b9cb3986df26
ihalapathirana <anusha.ihalapathirana@trycake.com>
2017-12-23 18:04:04 +0530

    changed port

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,11 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
+connectionLimit : 10,
 host:'173.194.224.165',
 user:'root',
 password:'Anusha@123',
 database:'PhoneAPI'
 });
-
+ 
 module.exports=connection;
